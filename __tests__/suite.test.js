1cd8393ed0f4dd0a6cc6563677624e70d70889e3
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-09-30 15:46:01 -0500

    chore: added instructions and test cases

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,8 @@
-const { func } = require('../exercise');
+const { hard } = require('../exercise');
 
 test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
+    expect(hard([1,2,3,3], [3,4,5,6], [50,10,20,70])).toBeDefined();
+    expect(hard([1,2,3,3], [3,4,5,6], [50,10,20,70])).toBe(120);
+    expect(hard([1,2,3,4,6], [3,5,10,6,9], [20,20,100,70,60])).toBe(150);
+    expect(hard([1,1,1], [2,3,4], [5,6,4])).toBe(6);
  })
--- exercise.js
+++ exercise.js
@@ -1,5 +1,28 @@
+/*
+
+We have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+ 
+For ex.
+
+startTime = [1,2,3,3]
+endTime = [3,4,5,6]
+profit = [50,10,40,70]
+
+the result should be: 120
+
+This is because startTime[0] - endTime[0] gives a profit of profit[0] (50) and
+startTime[3] - endTime[3] gives a profit of 70 so 50 + 70 = 120.
+
+
+
+
 /** DO NOT CHANGE THE FUNCTION NAME **/
-const func = (c) => {
+const hard = (startTime, endTime, profit) => {
     /* Only make changes below this comment */
     
     
@@ -11,4 +34,4 @@ const func = (c) => {
 
 
 /** DO NOT CHANGE THE LINE BELOW **/
-module.exports.func = func;
+module.exports.hard = hard;
